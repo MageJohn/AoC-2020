105d0f2c9e3e6820436deaae86b6a6a3311109a9
Yuri Pieters <magejohnyjtp@gmail.com>
2021-12-04 00:06:58 +0000

    Update doc comment in helpers.zig

--- helpers.zig
+++ helpers.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
-// Inline means the memory is in the callers stack frame.
+/// Reads the input file into an array of size max_len and returns the data as
+/// a slice. The function is inlined so the memory is allocated on the callers
+/// stack, so does not require an allocator.
 pub inline fn readInput(path: []const u8, comptime max_len: usize) ![]const u8 {
     const input_file = try std.fs.cwd().openFile(path, .{});
     defer input_file.close();
